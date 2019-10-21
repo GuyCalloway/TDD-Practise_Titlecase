78890339f6afce68828d7836ab077e6a2a54f271
GuyCalloway <guyfcalloway@gmail.com>
2019-10-22 00:41:15 +0100

    fourth Test passing

--- lib/titlecase.rb
+++ lib/titlecase.rb
@@ -1,3 +1,18 @@
 def title_case(string, exceptions = "")
-    return string.split(" ").map! {|str| str.capitalize}.join(" ")
+    exceps = exceptions.split(" ").each{ |x| x.downcase!}
+    p exceps
+    array = string.split(" ")
+    title_array = array.each_with_index {|str, i| str.downcase!
+                            if i == 0
+                                str.capitalize!
+                            elsif !exceps.include?(str)
+                                str.capitalize!
+                            else
+                                str
+                            end            
+                                            
+                                                    
+                            }     
+                               p array
+    return array.join(" ")             
 end
--- spec/titlecase_spec.rb
+++ spec/titlecase_spec.rb
@@ -5,7 +5,15 @@ describe 'titlecase' do
         expect(title_case("Hello")).to eq('Hello')
     end
 
-    it "returns string" do
+    it "returns capitalized words" do
         expect(title_case("hello there")).to eq('Hello There')
+    end
+
+    it "returns string with exceptions left out" do
+        expect(title_case('THE WIND IN THE WILLOWS', 'The In')).to eq('The Wind in the Willows')
+    end
+
+    it "returns string all capitalized" do
+        expect(title_case('the quick brown fox')).to eq('The Quick Brown Fox')
     end
 end
