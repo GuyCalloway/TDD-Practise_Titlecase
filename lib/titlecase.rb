d3a56338f895b7d5df8b430590ffe181aa90312b
GuyCalloway <guyfcalloway@gmail.com>
2019-10-22 15:07:07 +0100

    Reordered if else statement

--- lib/titlecase.rb
+++ lib/titlecase.rb
@@ -1,18 +1,15 @@
 def title_case(string, exceptions = "")
     exceps = exceptions.split(" ").each{ |x| x.downcase!}
-    p exceps
     array = string.split(" ")
-    title_array = array.each_with_index {|str, i| str.downcase!
-                            if i == 0
+    title_array = array.each_with_index {|str, i| str.downcase! 
+                            if !exceps.include?(str)
                                 str.capitalize!
-                            elsif !exceps.include?(str)
-                                str.capitalize!
+                            elsif i == 0
+                                    str.capitalize!
                             else
                                 str
-                            end            
-                                            
-                                                    
+                            end                              
                             }     
-                               p array
+                              
     return array.join(" ")             
 end
